97e55a7e9995746bf8a80553730a037171eede0a
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-17 10:46:49 +0600

    added image in banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,13 @@ const Header = () => {
     return (
         <header>
             <section className="">
-            <img className="absolute top-0 left-0 right-0" src={banner} alt="" />
+            <img className="absolute top-0 left-0 right-0 w-full object-cover" src={banner} alt="" />
             </section>
+
+            <div className="before:content-[''] w-full  relative before:absolute before:bg-[#ffffffe5] before:top-0 before:right-0 before:left-0 before:bottom-0">
+
+                <img className="w-full   object-cover" src={banner} alt="" />
+            </div>
 
             <Navbar/>
         </header>
